9a317ae0fb7b1370fbe56e125c304377aad7ba14
Norman Libiran <n_libiran@yahoo.com>
2023-04-13 09:03:10 +0000

    Updated page layouts.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
+
+# Dashboard
 @app.route("/")
-def home():
-    return render_template('home.html')
+def dashboard():
+  return render_template("dashboard.html", now=datetime.utcnow())
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
